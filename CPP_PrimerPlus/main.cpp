46db0d29eee897614df68632557abb1242f2327c
dragon big <1363992060@qq.com>
2023-08-17 11:36:35 +0800

    20220817

--- CPP_PrimerPlus/main.cpp
+++ CPP_PrimerPlus/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <ctime>
 
@@ -6,7 +5,6 @@ using namespace std;
 //5.1 for循环
 void for_loop()
 {
-
 	//----------5.3 express.cpp
 	cout << "the expression x = 100 has the value ";
 	int x;
@@ -28,10 +26,8 @@ void for_loop()
 	cin >> limit;
 	int i;
 	//注意C++好像没有使用逗号的
-	for (i = limit ; i ; i--)
+	for (i = limit; i; i--)
 		cout << "i = " << i << "\n";
-
-
 }
 
 //5.2 while循环
@@ -45,15 +41,16 @@ void whiile_loop()
 	do
 	{
 		cout << "猜错了" << endl;
-		if (guessNumber>targetNumber)
+		if (guessNumber > targetNumber)
 		{
 			guessNumber--;
 		}
 		else
 		{
-						guessNumber++;
+			guessNumber++;
 		}
-	} while (guessNumber != targetNumber);
+	}
+	while (guessNumber != targetNumber);
 	cout << "猜对了，是7" << endl;
 
 	//----------5.14 waiting.cpp
@@ -78,15 +75,15 @@ void whiile_loop()
 
 	while (name[i] != '\0')
 	{
-		cout << "name[" << i << "] is " << name[i] <<" ascii :"<<(int)name[i] << endl;
-		i ++;
+		cout << "name[" << i << "] is " << name[i] << " ascii :" << (int)name[i] << endl;
+		i++;
 	}
 }
 
 //5.4 基于范围的for循环
 void forLoopBaseRange()
 {
-	int array[5] = { 1,2,3,4,5 };
+	int array[5] = {1, 2, 3, 4, 5};
 	for (auto element : array)
 	{
 		cout << element << endl;
@@ -102,19 +99,17 @@ void forLoopBaseRange()
 	{
 		cout << element << endl;
 	}
-
 }
 
 //5.5 循环和文本输入
-void  loopTxtInput()
+void loopTxtInput()
 {
-
 	//--------5.18 textin3.cpp
 	char input1;
 	cout << "enter any charactor to continue, and S+Z to end" << endl;
 	cin.get(input1);
 	int count1 = 0;
-	while ( cin.fail() == false)
+	while (cin.fail() == false)
 	{
 		cout << input1;
 		//cin.get 和 cin >>的区别
@@ -138,10 +133,38 @@ void  loopTxtInput()
 	cout << "the end count is " << count << endl;
 }
 
+void functionAndArray()
+{
+	auto n_char = [](char inputChar, int num)
+	{
+		while (num-- > 0)
+		{
+			cout << inputChar << "\t";
+		}
+	};
+	int time;
+	char inputChar;
+	cout << "enter char" << std::endl;
+	cin >> inputChar; // 读取输入的字符
+
+	while (inputChar != 'q')
+	{
+		cout << "enter print num" << std::endl;
+		cin >> time;
+		n_char(inputChar, time);
+		cout << "enter new inputChar" << endl;
+		cin >> inputChar;
+	}
+}
+
 int main()
 {
+ 	// ------------------7.3函数和数组
+	functionAndArray();
+
+
 	//----------5.5 循环和文本输入
-	loopTxtInput();
+	// loopTxtInput();
 
 	//----------5.4 基于范围的for循环
 	//forLoopBaseRange();
